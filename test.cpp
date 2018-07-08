e6741d988b2ebf778ff73b1b41a25f11c8711143
luyongyi <lu452803964@qq.com>
2018-07-09 01:21:18 +0800

    Update test.cpp
    time test

--- test.cpp
+++ test.cpp
@@ -1,6 +1,19 @@
+
 #include <iostream>
-#include <wiringpi>
+#include <ctime>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+using namespace std;
+
 int main()
 {
-}
-
+    struct timeval tv, ts;
+    gettimeofday(&tv, NULL);
+    sleep(3);
+    gettimeofday(&ts, NULL);
+    cout << "second" << tv.tv_sec - ts.tv_sec << endl;
+    cout << "milsec:" << tv.tv_sec * 1000 + tv.tv_usec / 1000 - ts.tv_sec * 1000 - ts.tv_usec / 1000 << endl;
+    
+  }
