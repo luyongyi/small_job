f9ab84cbf8ec026cc30a847027a5608fc884e780
luyongyi <lu452803964@qq.com>
2018-07-09 01:28:13 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,10 +1,9 @@
-
 #include <iostream>
 #include <ctime>
 #include <stdlib.h>
 #include <sys/time.h>
 #include <unistd.h>
-
+#include <cstdio>
 using namespace std;
 
 int main()
@@ -15,5 +14,5 @@ int main()
     gettimeofday(&ts, NULL);
     cout << "second" << tv.tv_sec - ts.tv_sec << endl;
     cout << "milsec:" << tv.tv_sec * 1000 + tv.tv_usec / 1000 - ts.tv_sec * 1000 - ts.tv_usec / 1000 << endl;
-    
-  }
+    printf("millisecond:%ld\n",tv.tv_sec*1000 + tv.tv_usec/1000-ts.tv_sec*1000-ts.tv_usec/1000);
+    }
