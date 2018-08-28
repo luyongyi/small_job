1cc2ad92be93083551d0916149c482b71a47bbdc
luyongyi <lu452803964@qq.com>
2018-08-28 18:14:12 +0800

    Create humen.cpp

---
+++ humen.cpp
@@ -0,0 +1,14 @@
+#include <wiringPi.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    pinMode(1, INPUT);
+
+    while (1)
+    {
+        int i = 0;
+        cout << digitalRead(1) << endl;
+    }
+}
